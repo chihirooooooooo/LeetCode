cceb81711b379bfb75a9c04971516f1b8020b1b1
chihirooooooooo <107065792+chihirooooooooo@users.noreply.github.com>
2023-01-21 20:00:29 +0900

    Create 1480-Running-Sum-of-1d-Array.cpp

---
+++ 1480-Running-Sum-of-1d-Array/1480-Running-Sum-of-1d-Array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> ans;
+        int temp = nums[0];
+        ans.push_back(nums[0]);
+        for(int i = 1; i < nums.size(); i++){
+            temp += nums[i];
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
